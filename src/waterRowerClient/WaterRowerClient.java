9b424ef805389018b119640c09a23ea621020adb
cp <cp.eclipsing@googlemail.com>
2019-07-09 21:15:16 +0200

    first class

---
+++ src/waterRowerClient/WaterRowerClient.java
@@ -0,0 +1,14 @@
+package waterRowerClient;
+
+public class WaterRowerClient {
+
+	public WaterRowerClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
